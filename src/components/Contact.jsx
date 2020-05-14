e39ed18e617e59fe98b511b6a566201a508200dd
Isabelly <isabelly.paganini@gmail.com>
2020-05-14 10:22:16 -0300

    created component: Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    const { data } = this.props;
+    const admissionDate = new Date(data.admissionDate).toLocaleDateString();
+
+    return (
+      <article className="contact" key={data.id} data-testid="contact">
+        <span className="contact__avatar">
+          <img src={data.avatar} alt={data.name} />
+        </span>
+        <span className="contact__data">{data.name}</span>
+        <span className="contact__data">{data.phone}</span>
+        <span className="contact__data">{data.country}</span>
+        <span className="contact__data">{admissionDate}</span>
+        <span className="contact__data">{data.company}</span>
+        <span className="contact__data">{data.department}</span>
+      </article>
+    );
   }
 }
 
