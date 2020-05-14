79226195d5272933aa4ff3fe8fc0d3cc49f6e27d
Isabelly <isabelly.paganini@gmail.com>
2020-05-14 11:24:27 -0300

    adjusted test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
+import dayjs from "dayjs";
 
 class Contact extends Component {
   render() {
     const { data } = this.props;
-    const admissionDate = new Date(data.admissionDate).toLocaleDateString();
 
     return (
       <article className="contact" key={data.id} data-testid="contact">
         <span className="contact__avatar">
           <img src={data.avatar} alt={data.name} />
         </span>
-        <span className="contact__data">{data.name}</span>
-        <span className="contact__data">{data.phone}</span>
-        <span className="contact__data">{data.country}</span>
-        <span className="contact__data">{admissionDate}</span>
-        <span className="contact__data">{data.company}</span>
-        <span className="contact__data">{data.department}</span>
+        <span className="contact__data" data-testid="contact-name">{data.name}</span>
+        <span className="contact__data" data-testid="contact-phone">{data.phone}</span>
+        <span className="contact__data" data-testid="contact-country">{data.country}</span>
+        <span className="contact__data" data-testid="contact-date">{dayjs(data.admissionDate).format("DD/MM/YYYY")}</span>
+        <span className="contact__data" data-testid="contact-company">{data.company}</span>
+        <span className="contact__data" data-testid="contact-department">{data.department}</span>
       </article>
     );
   }
