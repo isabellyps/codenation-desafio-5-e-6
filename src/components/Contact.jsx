7c168f2dd3691e626bc7966bd55b8b7ecda3ba31
Isabelly <isabelly.paganini@gmail.com>
2020-05-14 10:23:44 -0300

    created component: Contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Contact extends React.Component {
+class Contact extends Component {
   render() {
     const { data } = this.props;
     const admissionDate = new Date(data.admissionDate).toLocaleDateString();
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import Contact from "./Contact";
 
-class Contacts extends React.Component {
-	render() {
-		return (<div />);
-	}
+class Contacts extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  contactsResultItems: [],
+		};
+	  }
+	
+	  changeContacts() {
+		ApiService.ListContacts().then((res) => {
+		  const contactsMap = res.map((item) => {
+			return <Contact key={item.id} data={item} />;
+		  });
+		  this.setState({ contactsResultItems: [""] });
+		  this.setState({
+			contactsResultItems: [
+			  ...this.state.contactsResultItems,
+			  ...contactsMap,
+			],
+		  });
+		});
+	  }
+	
+	  componentDidMount() {
+		this.changeContacts();
+	  }
+	
+	  render() {
+		return (
+		  <div className="container" data-testid="contacts">
+			<section className="contacts">
+			  <article className="contact">
+				<span className="contact__avatar" />
+				<span className="contact__data">Nome</span>
+				<span className="contact__data">Telefone</span>
+				<span className="contact__data">País</span>
+				<span className="contact__data">Admissão</span>
+				<span className="contact__data">Empresa</span>
+				<span className="contact__data">Departamento</span>
+			  </article>
+			  {this.state.contactsResultItems}
+			</section>
+		  </div>
+		);
+	  }
 }
 
 export default Contacts;
