9ce6930c71d5bf6087495038ffdad278cbaf7624
Isabelly <isabelly.paganini@gmail.com>
2020-05-14 10:24:55 -0300

    created component: Topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from "react";
+import { ReactComponent as LogoSvg } from "../assets/img/logo.svg";
 
-class Topbar extends React.Component {
-	render() {
-		return (<div />);
-	}
+class Topbar extends Component {
+  render() {
+    return (
+      <header className="topbar" data-testid="topbar">
+        <div className="container">
+          <a href="/" className="topbar__logo">
+            <LogoSvg alt="Logo" />
+          </a>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Topbar;
